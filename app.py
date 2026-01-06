4b07c7b7e168bb09aa197b4d6aff0857dd025fca
kacper <kacper.ogrodowicz02@gmail.com>
2026-01-06 18:40:58 +0100

    oddzielne pipline'y, baza danych, dwa kontenery w docker compose

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import os
+import redis
 
 app = Flask(__name__)
 
+redis_host = os.environ.get('REDIS_HOST', 'redis')
+cache = redis.Redis(host=redis_host, port=6379)
+
+def get_hit_count():
+    try:
+        return cache.incr('hit')
+    except redis.exceptions.ConnectionError:
+        return 0
+
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    count = get_hit_count()
+    return render_template('index.html', visit_count=count)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
